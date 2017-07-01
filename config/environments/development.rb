2b40f10f386fd65538007ed07984ff9c65f19d0c
gauravano <sachdeva.gaurav1997@gmail.com>
2017-07-01 18:54:29 +0530

    devise working successfully

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,8 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -38,4 +40,17 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'sachdevarockz@gmail.com',
+    password:             'awesome1997',
+    authentication:       :login,
+    enable_starttls_auto: true
+  }
 end
